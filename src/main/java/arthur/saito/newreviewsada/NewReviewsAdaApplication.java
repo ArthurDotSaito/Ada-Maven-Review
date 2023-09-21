1b35d9f5ea9f2c740cfcca20d5be47e634410bd4
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-20 23:54:13 -0300

    starting project

---
+++ src/main/java/arthur/saito/newreviewsada/NewReviewsAdaApplication.java
@@ -0,0 +1,13 @@
+package arthur.saito.newreviewsada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewReviewsAdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewReviewsAdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arthur/saito/newreviewsada/NewReviewsAdaApplicationTests.java
@@ -0,0 +1,13 @@
+package arthur.saito.newreviewsada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewReviewsAdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
