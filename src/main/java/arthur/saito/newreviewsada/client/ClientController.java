b59e65f342aab20d9acda3a9a16f926eebeda8e7
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-22 00:51:12 -0300

    feat: adding a client controller layer

--- src/main/java/arthur/saito/newreviewsada/client/ClientController.java
+++ src/main/java/arthur/saito/newreviewsada/client/ClientController.java
@@ -1,4 +1,20 @@
 package arthur.saito.newreviewsada.client;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
 public class ClientController {
+    private final ClientService service;
+
+    public ClientController(ClientService service){
+        this.service = service;
+    }
+
+     public List<Client> listAll(){
+        return this.service.listAll();
+     }
 }
