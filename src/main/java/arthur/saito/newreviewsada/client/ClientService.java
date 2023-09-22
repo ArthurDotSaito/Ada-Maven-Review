ad8a2920ca5ab22dd741245cc5cbca7ce15c2816
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-22 00:50:56 -0300

    feat: adding a client service layer

--- src/main/java/arthur/saito/newreviewsada/client/ClientService.java
+++ src/main/java/arthur/saito/newreviewsada/client/ClientService.java
@@ -1,4 +1,17 @@
 package arthur.saito.newreviewsada.client;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ClientService {
+    private final ClientJPARepository repository;
+
+    public ClientService(ClientJPARepository repository) {
+        this.repository = repository;
+    }
+    public List<Client> listAll(){
+        return this.repository.findAll();
+    }
 }
