e8c830bd9f39458c46e680411d59e0bf8caf07b0
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-25 00:03:57 -0300

    chore: adding a modelMapper dependency

---
+++ src/main/java/arthur/saito/newreviewsada/config/ModelMapperConfig.java
@@ -0,0 +1,11 @@
+package arthur.saito.newreviewsada.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
